0998b70a61cfb0ddc83262b53fa24efb4987a5b1
kbsonlong <kbsonlong@gmail.com>
2018-01-23 18:06:27 +0800

    Jquery study

---
+++ static/js/show_btc.js
@@ -0,0 +1,41 @@
+function show_btc (data) {
+    var packJson = data;
+    var lists = packJson.data.list;
+    for(var i = 0,j=1; i < lists.length; i++,j++){
+        //将数据追加到id为hq的p标签
+        if (lists[i].zhName==""){
+            $("table").append("<tbody id='btc'><tr>" +
+           "<td>" + j +"</td>" +
+           "<td>" + lists[i].symbol + "-" + lists[i].name + "</td>" +
+           "<td>" + lists[i].price +"</td>" +
+           "<td>" + lists[i].change1h +"</td>" +
+           "<td>" + lists[i].change1d +"</td>" +
+           "<td>" + lists[i].volume_ex +"</td>" +
+           "<td>" + lists[i].marketCap +"</td>" +
+           "<td>" + lists[i].change7d +"</td>" +
+           "<td>" + lists[i].change7d +"</td>" +
+           "</tr></tbody>");}
+        else {
+           $("table").append("<tbody id='btc'><tr>" +
+               "<td>" + j +"</td>" +
+               "<td>" + lists[i].symbol + "-" + lists[i].zhName + "</td>" +
+               "<td>" + lists[i].price +"</td>" +
+               "<td>" + lists[i].change1h +"</td>" +
+               "<td>" + lists[i].change1d +"</td>" +
+               "<td>" + lists[i].volume_ex +"</td>" +
+               "<td>" + lists[i].marketCap +"</td>" +
+               "<td>" + lists[i].change7d +"</td>" +
+               "</tr></tbody>");}
+    }
+}
+
+
+
+function getTime(){
+        var myDate = new Date();
+        var date = myDate.toLocaleDateString();
+        var hours = myDate.getHours();
+        var minutes = myDate.getMinutes();
+        var seconds = myDate.getSeconds();
+        $("#showDate").html(date+" "+hours+":"+minutes+":"+seconds);
+    }
